dea37fad846b5fee8ab0462ef91cfe28b6f85a9e
Rusman <rusman84@hotmail.es>
2020-07-13 09:36:16 -0400

    Add project files.

---
+++ Programa/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1= new Student();
+            student1.name = "jose";
+            student1.grade = 72.5;
+            student1.year = 2020;
+
+            Console.WriteLine(student1.name);
+
+            Student student2 = new Student("Gustavo Huanca", 2020, 95);
+            student2.name = "Gustavo H.";
+            Console.WriteLine(student2.name);
+            
+        }
+    }
+}
---
+++ Programa/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programa
+{
+    public class Student
+    {
+        //variables glovales
+        public string name;
+        public int year;
+        public double grade;
+
+        public Student()
+        {
+            name = "";
+            year = 1900;
+            grade = 0;
+        }
+
+        public Student(string name,int year, double grade)
+        {
+            //variable locales
+            this.name = name;
+            this.year = year;
+            this.grade = grade;
+        }
+    }   
+}
